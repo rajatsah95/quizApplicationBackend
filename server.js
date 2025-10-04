3afdfe2a18b51c8dbb721ded5999dcee7176d8c8
rajatsah95 <rajatsah95@gmail.com>
2025-10-04 20:16:47 +0530

    quiz app added 2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const quizRoutes = require('./routes/quizRoutes');
 
 const app = express();
-app.use(cors({origin:"https://emaildetailer.netlify.app"}));
+app.use(cors({origin:"https://elaborate-bunny-7e1aa2.netlify.app"}));
 app.use(express.json());
 
 app.use('/api/quizzes', quizRoutes);
